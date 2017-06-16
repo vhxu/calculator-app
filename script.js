422e640329af6706f47b2c3b4e1365daa85d2d36
Virgil Xu <virgilxu89@gmail.com>
2017-06-16 01:18:08 -0700

    added a index.html file with buttons. Buttons are linked to functions in script file

--- script.js
+++ script.js
@@ -12,5 +12,26 @@ var calculator = {
   doMath: function() {
     var equation = this.calculator.join('');
     console.log(eval(equation));
+  },
+  clear: function() {
+    this.calculator.splice(0,this.calculator.length);
+    console.log(this.calculator);
+  }
+};
+
+var handlers = {
+  addValues: function(value) {
+    //var addValueInput = document.getElementById('test');
+    calculator.addValues(value);
+    // addValueInput.value = '';
+  },
+  equals: function() {
+    calculator.doMath();
   }
 }
+
+//AC, +/-, %, /
+//7, 8, 9 , x
+//4, 5, 6, -
+//1, 2, 3, +
+//0, ., =
